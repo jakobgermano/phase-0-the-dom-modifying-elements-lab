e29b0aafa621fe6a472dbc92770561d46c24ce6c
jakob <jakob.germano@gmail.com>
2021-03-27 12:17:35 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "Victory")
+
+newHeader.innerHTML = "MIKELANNE is the champion";
+
+document.body.appendChild(newHeader);
+
+
+
